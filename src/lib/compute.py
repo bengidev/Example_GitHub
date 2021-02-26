78446b68738bd1839c3ad730c471f16f871122f2
syndicate017 <geniuses17@gmail.com>
2021-02-26 23:16:38 +0700

    Add some features of a class for Math Functions

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -1,2 +1,27 @@
 #Example of file "compute" which created using Python language programming.
 
+
+# Exercise 1: Viewing and Establishing the Status of a File
+
+
+class Compute:
+    def __init__(self, operator, operands):
+        self.operator = operator
+        self.operands = operands
+
+
+    def add(self):
+        pass
+
+
+    def subtract(self):
+        pass
+
+
+    def divide(self):
+        pass
+
+
+    def multiply(self):
+        pass
+
